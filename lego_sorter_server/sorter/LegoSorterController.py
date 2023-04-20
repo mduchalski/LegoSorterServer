41aaa790cb32a71519aa5aca5aa8f4e7d9a014a6
Mateusz Duchalski <mduchalski1@gmail.com>
2023-04-20 09:15:37 +0200

    Add envvars for sorter addresses

--- lego_sorter_server/sorter/LegoSorterController.py
+++ lego_sorter_server/sorter/LegoSorterController.py
@@ -1,3 +1,4 @@
+import os
 import logging
 
 import requests
@@ -6,25 +7,27 @@ from lego_sorter_server.service.BrickCategoryConfig import BrickCategoryConfig
 
 
 class LegoSorterController:
-    CONVEYOR_LOCAL_ADDRESS = "http://192.168.83.45:8000"
-    SORTER_LOCAL_ADDRESS = "http://192.168.83.45:8001"
+    CONVEYOR_LOCAL_ADDRESS = os.getenv('CONVEYOR_LOCAL_ADDRESS')
+    SORTER_LOCAL_ADDRESS = os.getenv('SORTER_LOCAL_ADDRESS')
 
     def __init__(self, brickCategoryConfig: BrickCategoryConfig):
         self.speed = 50
         self.brickCategoryConfig = brickCategoryConfig
 
     def run_conveyor(self):
-        requests.get(f"{self.CONVEYOR_LOCAL_ADDRESS}/start?duty_cycle={self.speed}")
+        if self.CONVEYOR_LOCAL_ADDRESS != None:
+            requests.get(f"{self.CONVEYOR_LOCAL_ADDRESS}/start?duty_cycle={self.speed}")
 
     def stop_conveyor(self):
-        requests.get(f"{self.CONVEYOR_LOCAL_ADDRESS}/stop")
+        if self.CONVEYOR_LOCAL_ADDRESS != None:
+            requests.get(f"{self.CONVEYOR_LOCAL_ADDRESS}/stop")
 
     def on_brick_recognized(self, brick):
         brick_coords, brick_cls, brick_prob = brick
         cat_name, pos = self.brickCategoryConfig[brick_cls]
         logging.info(f"Moving brick with class: {brick_cls} to stack: {cat_name} (pos: {pos})")
-        # TODO below commented out temporarily to support data collection
-        #requests.get(f"{self.SORTER_LOCAL_ADDRESS}/sort?action={pos}")
+        if self.SORTER_LOCAL_ADDRESS != None:
+            requests.get(f"{self.SORTER_LOCAL_ADDRESS}/sort?action={pos}")
 
     def set_machine_speed(self, speed):
         self.speed = speed
